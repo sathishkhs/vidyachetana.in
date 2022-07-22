962f5458cb18ed9fc1bed5e113ba80e7271220a5
sathish kumar <67854806+sathishds@users.noreply.github.com>
2022-07-22 19:58:40 +0530

    slider responsive

--- front/screens/templates/includes/slider.php
+++ front/screens/templates/includes/slider.php
@@ -1,75 +1,30 @@
-<section class="main-slider">
-    <div class="swiper-container thm-swiper__slider" data-swiper-options='{"slidesPerView": 1, "loop": true,
-    "effect": "fade",
-     "pagination": {
-        "el": "#main-slider-pagination",
-        "type": "bullets",
-        "clickable": true
-      },
-    "navigation": {
-        "nextEl": "#main-slider__swiper-button-next",
-        "prevEl": "#main-slider__swiper-button-prev"
-    },
-    "autoplay": {
-        "delay": 9000
-    }}'>
-        <div class="swiper-wrapper">
-          
-            <div class="swiper-slide">
-                <div class="image-layer" >
-                    <img src="assets/images/backgrounds/homepage banner_blog.jpg" class="w-100" alt="banner">
-                </div>
-                <a href="blog/post/vidya-chetana-employability-skills-world-youth-skills-day">
-                    <div class="image-layer-overlay"></div>
-                </a>
-               
-            </div>
-          
-            <div class="swiper-slide">
-                <div class="image-layer" >
-                    <img src="assets/images/backgrounds/vc_banner-11.jpg" class="w-100" alt="banner">
-                </div>
-                <a href="each-1-educate-1">
-                    <div class="image-layer-overlay"></div>
-                </a>
-               
-            </div>
-            <div class="swiper-slide">
-                <div class="image-layer" >
-                    <img src="assets/images/backgrounds/faq-banner.jpg" class="w-100" alt="banner">
-                </div>
-                <a href="faq">
-                    <div class="image-layer-overlay"></div>
-                </a>
-               
-            </div>
-            <div class="swiper-slide">
-                <div class="image-layer" >
-                    <img src="assets/images/backgrounds/csr-banner.jpg" class="w-100" alt="banner">
-                </div>
-                <a href="csr-partnership">
-                    <div class="image-layer-overlay"></div>
-                </a>
-               
-            </div>
-            <!--<div class="swiper-slide">-->
-            <!--    <div class="image-layer" >-->
-            <!--        <img src="assets/images/backgrounds/qa-page-banner.png" class="w-100" alt="banner">-->
-            <!--    </div>-->
-            <!--    <a href="each-1-educate-1">-->
-            <!--        <div class="image-layer-overlay"></div>-->
-            <!--    </a>-->
-               
-            <!--</div>-->
-      
-        <!-- If we need navigation buttons -->
-        <div class="swiper-pagination" id="main-slider-pagination"></div>
-        <div class="main-slider__nav">
-            <div class="swiper-button-prev" id="main-slider__swiper-button-next"><i class="icon-right-arrow icon-left-arrow"></i>
-            </div>
-            <div class="swiper-button-next" id="main-slider__swiper-button-prev"><i class="icon-right-arrow"></i>
-            </div>
-        </div>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/images/backgrounds/homepage banner_blog.jpg" class="d-block w-100" alt="...">
     </div>
-</section>
-
+    <div class="carousel-item">
+      <img src="assets/images/backgrounds/vc_banner-11.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/backgrounds/faq-banner.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/backgrounds/csr-banner.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
